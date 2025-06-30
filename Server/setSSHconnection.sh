7da8ac53b742392cf783aeb5ea0793abcfa63ebc
Pylyp-Roboneers <pylyp.volodin@roboneers.net>
2025-06-30 17:44:03 +0300

    correction of ssh connection creation

--- Server/setSSHconnection.sh
+++ Server/setSSHconnection.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "SSH connection"
-REMOTE_ADDRESS=deck@10.168.103.8
-keyFile=keyToClnt08
+#REMOTE_ADDRESS=deck@10.168.103.8
+#keyFile=keyToClnt08
+if [  -z "$1" ]; then echo -e "no remote user and wireguard address are specified as first argumet.\nExit" ; exit 1; fi
+REMOTE_ADDRESS=$1
+if [  -z "$2" ]; then echo -e "no ssh key name is specified as first argumet.\nExit" ; exit 1; fi
+keyFile=$2
 
 # Generate ssh key pair
 cd /home/pi/.ssh/
@@ -12,6 +16,9 @@ if [ $? -ne 0 ];
 fi
 sudo chmod 600 $keyFile 
 sudo chmod 600 $keyFile.pub 
+sudo chown pi:pi $keyFile
+sudo chown pi:pi $keyFile.pub
+
 ls -l
 
 # install ssh key on remote
